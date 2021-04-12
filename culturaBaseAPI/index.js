16ad45171666118f335fe04bda0af360e1194b2f
josemarimaya <josemarimaya@gmail.com>
2021-04-12 16:56:09 +0200

    cosas de culturaBASE

---
+++ culturaBaseAPI/index.js
@@ -0,0 +1,173 @@
+var initJsonData = require("./initialData");
+var culturaBASE_html = require("./l05");
+
+var BASE_CULTURABASE_API_PATH = "/api/v1/culturaBASE";
+
+var r_culturaBASE = [];
+
+module.exports.info = (app) => {
+
+    app.get("/info/culturaBASE", (req,res) => {
+        res
+        .status(200)
+        .send(culturaBASE_html.info_culturaBASE);
+        //Llamamos a la información del html desde la variable que invoca el html
+    });
+
+};
+//Cargamos el loadInitialData antes que cualquier test para que no haya error
+
+module.exports.loadInitData = (app) => {
+    app.get(BASE_AIR_ROUTES_API_PATH + "/loadInitialData", (req,res) => {
+
+    
+        cb = initJsonData.JsonInitialData;
+        console.log("   -Hemos cargado exitosamente culturaBASE");
+        res.status(201).json(cb);
+    });
+
+};
+
+//Métodos CRUD con tabla azul
+
+module.exports.httpCRUD = (app) => {
+
+    //Yequest de CRUD básicas para CULTURABASE
+    //GET
+    app.get(BASE_CULTURABASE_API_PATH, (req,res) => {
+        
+        res
+        .status(200)
+        .send(JSON.stringify(cb,null,2));
+    });
+
+    //POST
+    app.post(BASE_CULTURABASE_API_PATH, (req,res) => {
+        var newResource = req.body;
+        console.log(`   Hemos añadido un recurso nuevo <${JSON.stringify(newResource,null,2)}>`);
+        cb.push(newResource);
+
+        res.sendStatus(201);
+    });
+    //PUT
+    app.put(BASE_CULTURABASE_API_PATH, (req,res) => {
+        
+        console.log(`   Usa un put de verdad, pringao `);
+        res.sendStatus(405);
+    });
+
+    //DELETE
+    app.delete(BASE_CULTURABASE_API_PATH,(req,res) => {
+        
+        cb = [];
+        console.log(`  Database deleted`);
+        res.sendStatus(200);
+    });
+
+    // Request por recurso
+
+    app.get(BASE_CULTURABASE_API_PATH + "/:urlDistrict", (req,res) => {
+
+        var {urlDistrict} = req.params;    // == var urlDistrict = req.params.urlDistrict
+
+        var ls_data = [];
+
+        for (var i = 0 ; i < cb.length; i++){
+            if(cb[i].district == urlDistrict){
+                
+                ls_data.push(cb[i]);
+                
+            }
+        };
+
+        if(ls_data.length == 0){
+            res
+            .status(404)
+            .send('The resource doesn´t exist.')
+        }else{
+            
+            res.status(200).send(JSON.stringify(ls_data,null,2));
+        }
+    
+    });
+
+    app.get(BASE_CULTURABASE_API_PATH + "/:urlDistrict/:urlYear", (req,res) => {
+
+        var {urlDistrict} = req.params;
+        var {urlYear} = req.params;
+
+        var res_data = {}
+        var resourceFinded = false;
+        
+
+        for (var i = 0 ; i < cb.length; i++){
+            if(cb[i].district == urlDistrict && cb[i].year == urlYear){
+                
+                res_data = cb[i];
+                resourceFinded = true;
+                
+            }
+        };
+
+        if(!resourceFinded){
+            res
+            .status(404)
+            .send('The resource doesn´t exist.')
+        }else{
+            res
+            .status(200)
+            .send(JSON.stringify(res_data,null,2));
+        }
+    });
+
+
+    app.post(BASE_CULTURABASE_API_PATH + "/:urlDistrict/:urlYear", (req,res) => {
+        console.log(`Error: Usa un post en condiciones chaval `);
+        res.sendStatus(405);
+    });
+
+    app.delete(BASE_CULTURABASE_API_PATH + "/:urlDistrict", (req,res) => {
+        var {urlDistrict} = req.params;
+
+        const deleted = cb.find(resource => resource.district == urlDistrict );
+
+        if(deleted){
+            cb = air_routes_aux.filter(resource => resource.district != urlDistrict);
+            res.status(200).json({ message: `The resource with district : <${urlDistrict}> was deleted`})
+        }else{
+            res.status(404).json({ message: "District you are looking for does not exist "})
+        }
+    });
+
+
+    app.delete(BASE_CULTURABASE_API_PATH + "/:urlDistrict/:urlYear", (req,res) => {
+
+        var {urlDistrict} = req.params;
+        var {urlYear} = req.params;
+
+        const deleted = cb.find(resource => (resource.district == urlDistrict)&&(resource.year == urlYear));
+
+        if(deleted){
+            cb = cb.filter(resource => (resource.district != urlDistrict)||(resource.district == urlDistrict && resource.year != urlYear));
+            res.status(200).json({ message: `The resources with district : <${urlDistrict}> and year: <${urlYear}> were deleted`})
+        }else{
+            res.status(404).json({ message: "The resource you are looking for does not exist "})
+        }
+    });
+
+    app.put(BASE_CULTURABASE_API_PATH+ "/:urlDistrict/:urlYear", (req,res) => {
+
+        var {urlDistrict} = req.params;
+        var {urlYear} = req.params;
+        
+        const index = cb.findIndex(resource => (resource.district == urlDistrict)&&(resource.year == urlYear));
+
+        if(index == -1){
+            res.status(404).json({ message: "The resource you are looking for does not exist "});
+        }else{
+            cb[index]= req.body;
+            res.status(200).json(cb[index]);
+        }
+    });
+
+};
---
+++ culturaBaseAPI/l05.js
@@ -0,0 +1,69 @@
+var culturaBASE_html = `<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<h3> <a href=" http://estadisticas.mecd.gob.es/CulturaDynPx/culturabase/index.htm?type=pcaxis&path=/t20/p20/a2005/&file=pcaxis#"> Enlace de referencia </a>
+</h3>
+
+<table>
+    <colgroup>
+    <col style="background-color:#f2f2f2">
+    </colgroup>
+    <tr>
+    <th>district</th>
+    <th>year</th>
+    <th>fundraising</th>
+    <th>spectator</th>
+    <th>spending_per_view</th>
+    </tr>
+    <tr>
+    <td>Andalucia</td>
+    <td>2019</td>
+    <td>88.3</td>
+    <td>16.4</td>
+    <td>5.4</td>
+    </tr>
+    <tr>
+    <td>Andalucia</td>
+    <td>2018</td>
+    <td>82.0</td>
+    <td>15.1</td>
+    <td>5.4</td>
+    </tr>
+
+    <tr>
+    <td>Madrid</td>
+    <td>2019</td>
+    <td>134.3</td>
+    <td>20.7</td>
+    <td>6.5</td>
+    </tr>
+
+    <tr>
+    <td>Madrid</td>
+    <td>2018</td>
+    <td>127.8</td>
+    <td>19.3</td>
+    <td>6.6</td>
+    </tr>
+
+    <tr>
+    <td>Ceuta y Melilla</td>
+    <td>2019</td>
+    <td>0.6</td>
+    <td>0.1</td>
+    <td>5.1</td>
+    </tr>
+</table>
+
+</body>
+</html>`;
+
+exports.info_culturaBASE = culturaBASE_html;
